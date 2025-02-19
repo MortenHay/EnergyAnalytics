cf3519f5345034b5601f1b3c6225e84bfd90589a
Morten <33462750+MortenHay@users.noreply.github.com>
2025-02-19 15:21:09 +0100

    Add files via upload

---
+++ Assignment1/Main.py
@@ -0,0 +1,69 @@
+# %% Importing Libraries
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import cvxpy as cp
+from UsefulFunctions import PricesDK, LoadData
+
+# %% Load Data
+df_prices = LoadData()
+df_prices = PricesDK(df_prices)
+df_prices.head()
+# %% Task 1.1:
+### Plotting the average spot price for each year ###
+# Calculate the average spot price for each year
+annual_means = df_prices["Spot"].groupby(df_prices["HourDK"].dt.year).mean()
+# Make simple bar chart
+plt.figure()
+plt.bar(annual_means.index, annual_means * 1000)  # Convert to DKK/MWh
+# Make it pretty
+plt.title("Average energy price for each year")
+plt.xlabel("Year")
+plt.ylabel("Average price [DKK/MWh]")
+plt.tight_layout()
+plt.show()
+
+# %% Task 1.2:
+### Plotting the average hourly spot price for each year ###
+# Explicitly extract the year and hour from the datetime column
+df_prices["year"] = df_prices["HourDK"].dt.year
+df_prices["hour"] = df_prices["HourDK"].dt.hour
+## Make new dataframe with the average price for each year and hour
+# Columns are years, rows are hours
+hourly_means = pd.DataFrame(
+    columns=df_prices["year"].unique(), index=df_prices["hour"].unique()
+)
+# Calculate the hourly means for each year
+for year in hourly_means.columns:
+    hourly_means[year] = (
+        df_prices.loc[df_prices["year"] == year].groupby("hour")["Spot"].mean()
+    )
+# Copy the last row to make the final step look nice
+## Make plot
+plt.figure()
+# Plot each year separately as a step plot
+for c in hourly_means.columns:
+    plt.stairs(
+        hourly_means[c] * 1000,
+        range(hourly_means.index[0], hourly_means.index[-1] + 2),
+        label=c,
+    )
+# Make it pretty
+plt.legend()
+plt.title("Average electricity spot price for each hour of the day")
+plt.xlabel("Hour of the day")
+plt.ylabel("Average price [DKK/MWh]")
+plt.xlim(0, 24)
+plt.xticks(np.arange(0, 25, 2))
+plt.tight_layout()
+plt.show()
+# %% Task 2.1
+### Define the given parameters ###
+SOC_min = 0.1
+SOC_max = 1
+P_max = 5  # kW
+capacity = 10  # kWh
+eta_c = eta_d = 0.95
+X_0 = 0.5
+X_n = 0.5
---
+++ Assignment1/UsefulFunctions.py
@@ -0,0 +1,93 @@
+import pandas as pd
+import os
+
+
+def PricesDK(df_prices):
+
+    df_prices["Spot"] = df_prices["SpotPriceDKK"]
+
+    ### Calculate the fixed Tax column ###
+
+    df_prices["Tax"] = 0.8
+
+    ### Calculate the fixed TSO column ###
+
+    df_prices["TSO"] = 0.1
+
+    ### Add the DSO tariffs ###
+
+    # Create a new column with Low load values
+    df_prices["DSO"] = 0.15
+
+    # List of winter months
+    winter = [1, 2, 3, 10, 11, 12]
+    peak_hours = range(17, 22)
+
+    ## Add High load values when outside low load period
+    # Winter
+    df_prices.loc[
+        (df_prices["HourDK"]).dt.month.isin(winter)
+        & ((df_prices["HourDK"]).dt.hour >= 6),
+        "DSO",
+    ] = 0.45
+    # Summer
+    df_prices.loc[
+        ((df_prices["HourDK"]).dt.month.isin(winter) == False)
+        & ((df_prices["HourDK"]).dt.hour >= 6),
+        "DSO",
+    ] = 0.23
+    ## Add Peak load values to appropriate hours
+    # Winter
+    df_prices.loc[
+        (df_prices["HourDK"]).dt.month.isin(winter)
+        & ((df_prices["HourDK"]).dt.hour.isin(peak_hours)),
+        "DSO",
+    ] = 1.35
+    # Summer
+    df_prices.loc[
+        ((df_prices["HourDK"]).dt.month.isin(winter) == False)
+        & ((df_prices["HourDK"]).dt.hour.isin(peak_hours)),
+        "DSO",
+    ] = 0.60
+
+    ### Calculate VAT ###
+    # Danish VAT is 25%
+    df_prices["VAT"] = 0.25 * (
+        df_prices["SpotPriceDKK"]
+        + df_prices["Tax"]
+        + df_prices["TSO"]
+        + df_prices["DSO"]
+    )
+
+    ### Calculate Buy price ###
+    # Buy price is sum of all other prices
+    # This is equivalent to VAT * 5
+    df_prices["Buy"] = df_prices["VAT"] * 5
+
+    return df_prices
+
+
+def LoadData(filename="ElspotpricesEA.csv"):
+    ### Load electricity prices ###
+    price_path = os.path.join(os.getcwd(), filename)
+    df_prices = pd.read_csv(price_path)
+
+    ### Convert to datetime ###
+    df_prices["HourDK"] = pd.to_datetime(df_prices["HourDK"])
+    df_prices["HourUTC"] = pd.to_datetime(df_prices["HourUTC"])
+    df_prices["HourUTC"] = df_prices["HourUTC"].dt.tz_localize("UTC")
+    df_prices["HourDK"] = df_prices["HourUTC"].dt.tz_convert("CET")
+
+    ### Convert prices from DKK/MWh to DKK/kWh ###
+    df_prices["SpotPriceDKK"] = df_prices["SpotPriceDKK"] / 1000
+
+    ### Filter only DK2 prices ###
+    df_prices = df_prices.loc[df_prices["PriceArea"] == "DK2"]
+
+    ### Keep only the local time and price columns ###
+    df_prices = df_prices[["HourDK", "SpotPriceDKK"]]
+
+    ### Reset the index ###
+    df_prices = df_prices.reset_index(drop=True)
+
+    return df_prices
