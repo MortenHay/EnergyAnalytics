c78d5177e8a2713b90ec243844921972a8f515ad
MortenHay <haymorten@gmail.com>
2025-03-09 19:38:18 +0100

    update 3.3

--- Assignment1/Main.py
+++ Assignment1/Main.py
@@ -291,10 +291,10 @@ constraints = [
     p_d >= 0,
     imp >= 0,
     exp >= 0,
-    p_c <= d * P_max,
-    p_d <= (1 - d) * P_max,
-    imp == p_c + shortage,
-    exp == p_d + surplus,
+    p_c <= P_max,
+    p_d <= P_max,
+    imp == p_c - p_d + shortage,
+    exp == p_d - p_c + surplus,
     X[0] == C_0 + p_c[0] * eta_c - p_d[0] * eta_d_inv,
     X[EOD] == C_0,
     X[-1] == C_n,
@@ -310,3 +310,116 @@ print("solving")
 problem.solve()
 print("done")
 print(problem.status)
+
+# %%
+# Create the figure and axes objects for the two subplots
+pltrange = (4000, 4000 + 24 * 2)
+fig, (ax1, ax2, ax3, ax4, ax5) = plt.subplots(5, 1, figsize=(8, 6), sharex=True)
+fig.subplots_adjust(hspace=0.4)  # Adjust space between plots
+
+# Plot the prices in the top subplot (exclude the first and last hours)
+ax1.stairs(
+    buy[pltrange[0] : pltrange[1]],
+    range(len(buy) + 1)[pltrange[0] : pltrange[1] + 1],
+    label="Prices",
+    baseline=None,
+    color="darkgreen",
+    linewidth=2,
+)
+ax1.stairs(
+    sell[pltrange[0] : pltrange[1]],
+    range(len(sell) + 1)[pltrange[0] : pltrange[1] + 1],
+    label="Prices",
+    baseline=None,
+    color="darkblue",
+    linewidth=2,
+)
+ax1.set_xlabel("Hour", fontsize=12)
+ax1.set_ylabel("Price [DKK/kWh]", fontsize=12)
+ax1.set_title("Spot Prices Over Time", fontsize=14, fontweight="bold")
+ax1.grid(True, linestyle=":", linewidth=0.7, alpha=0.8)
+
+# Plot the power in the bottom subplot (exclude the first and last hours)
+ax2.stairs(
+    p_c.value[pltrange[0] : pltrange[1]],
+    range(len(p_d.value) + 1)[pltrange[0] : pltrange[1] + 1],
+    label="Charging Power",
+    baseline=None,
+    color="green",
+    linewidth=2,
+)
+ax2.stairs(
+    -p_d.value[pltrange[0] : pltrange[1]],
+    range(len(p_d.value) + 1)[pltrange[0] : pltrange[1] + 1],
+    label="Discharging Power",
+    baseline=None,
+    color="red",
+    linewidth=2,
+)
+ax2.set_xlabel("Hour", fontsize=12)
+ax2.set_ylabel("Power [kW]", fontsize=12)
+ax2.set_title("Battery Charging/Discharging Schedule", fontsize=14, fontweight="bold")
+ax2.legend(loc="upper center", fontsize=10, frameon=True, shadow=True, ncol=2)
+ax2.grid(True, linestyle=":", linewidth=0.7, alpha=0.8)
+
+# Plot the state of charge in the middle subplot (exclude the first and last hours)
+ax3.plot(
+    range(len(p_d.value))[pltrange[0] : pltrange[1] + 1],
+    np.insert(X.value, 0, C_0)[pltrange[0] : pltrange[1] + 1],
+    label="SOC evolution",
+    color="b",
+    marker="o",
+    linestyle="--",
+)
+ax3.set_xlabel("Hour", fontsize=12)
+ax3.set_ylabel("State of Charge [kWh]", fontsize=12)
+ax3.set_title("Battery State of Charge Over Time", fontsize=14, fontweight="bold")
+ax3.grid(True, linestyle=":", linewidth=0.7, alpha=0.8)
+
+ax4.stairs(
+    imp.value[pltrange[0] : pltrange[1]],
+    range(len(p_d.value) + 1)[pltrange[0] : pltrange[1] + 1],
+    label="Import",
+    baseline=None,
+    color="green",
+    linewidth=2,
+)
+ax4.stairs(
+    -exp.value[pltrange[0] : pltrange[1]],
+    range(len(p_d.value) + 1)[pltrange[0] : pltrange[1] + 1],
+    label="Export",
+    baseline=None,
+    color="red",
+    linewidth=2,
+)
+ax4.set_xlabel("Hour", fontsize=12)
+ax4.set_ylabel("Power [kW]", fontsize=12)
+ax4.set_title("Import/Export Schedule", fontsize=14, fontweight="bold")
+ax4.legend(loc="upper center", fontsize=10, frameon=True, shadow=True, ncol=2)
+ax4.grid(True, linestyle=":", linewidth=0.7, alpha=0.8)
+
+ax5.stairs(
+    surplus[pltrange[0] : pltrange[1]],
+    range(len(p_d.value) + 1)[pltrange[0] : pltrange[1] + 1],
+    label="Surplus",
+    baseline=None,
+    color="green",
+    linewidth=2,
+)
+ax5.stairs(
+    shortage[pltrange[0] : pltrange[1]],
+    range(len(p_d.value) + 1)[pltrange[0] : pltrange[1] + 1],
+    label="Shortage",
+    baseline=None,
+    color="red",
+    linewidth=2,
+)
+ax5.set_xlabel("Hour", fontsize=12)
+ax5.set_ylabel("Power [kW]", fontsize=12)
+ax5.set_title("Surplus/Shortage Schedule", fontsize=14, fontweight="bold")
+ax5.legend(loc="upper center", fontsize=10, frameon=True, shadow=True, ncol=2)
+ax5.grid(True, linestyle=":", linewidth=0.7, alpha=0.8)
+# Show the plot
+plt.tight_layout()
+plt.show()
+# %%
