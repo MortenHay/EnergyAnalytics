9ddd67dcaa98a5e7bcea1b282c77013bf977ef3c
MortenHay <haymorten@gmail.com>
2025-03-10 16:41:01 +0100

    merge

--- Assignment1/Main.py
+++ Assignment1/Main.py
@@ -182,10 +182,12 @@ ax3.set_ylabel("State of Charge [kWh]", fontsize=12)
 ax3.set_title("Battery State of Charge Over Time", fontsize=14, fontweight="bold")
 ax3.grid(True, linestyle=":", linewidth=0.7, alpha=0.8)
 
+ax3.set_xlim(pltrange)
+ax3.set_xticks(range(pltrange[0], pltrange[1] + 1, 12))
 # Show the plot
 plt.tight_layout()
 plt.show()
-# %% Task 2.2
+# %% Task 2.1 data analysis
 ### Create the dataframe with the optimization results ###
 df_arbitrage = pd.DataFrame(
     {
@@ -194,6 +196,7 @@ df_arbitrage = pd.DataFrame(
         "State of Charge [kWh]": X.value,
     }
 )
+## Calculate profits of each hour
 df_arbitrage["Profit [DKK]"] = (
     -df_arbitrage["Charging Power [kW]"] * prices
     + df_arbitrage["Discharging Power [kW]"] * prices
@@ -209,13 +212,23 @@ df_arbitrage.where(
 df_arbitrage["charge total [kW]"] = (
     df_arbitrage["Charging Power [kW]"] - df_arbitrage["Discharging Power [kW]"]
 )
-### Aggregate the results to yearly and daily level ###
 ## Yearly aggregation
 df_arbitrage_year = (
     df_arbitrage[["Profit [DKK]", "Charging Power [kW]", "Discharging Power [kW]"]]
     .groupby(df_arbitrage["Time"].dt.year)
     .sum()
 )
+plt.figure()
+plt.bar(df_arbitrage_year.index, df_arbitrage_year["Profit [DKK]"])
+plt.title("Yearly profit from arbitrage")
+plt.xlabel("Year")
+plt.ylabel("Profit [DKK]")
+plt.grid(axis="y", linestyle=":", linewidth=0.7, alpha=0.8)
+plt.yticks(np.arange(0, 4501, 500))
+plt.tight_layout()
+plt.show()
+
+# %% Task 2.2
 ## Daily aggregation
 df_arbitrage_day = (
     df_arbitrage[["Profit [DKK]", "Charging Power [kW]", "Discharging Power [kW]"]]
@@ -251,6 +264,10 @@ df_arbitrage_day["price min"] = (
 df_arbitrage_day["price span"] = (
     df_arbitrage_day["price max"] - df_arbitrage_day["price min"]
 )
+## Coefficient of variance
+df_arbitrage_day["price cov"] = (
+    df_arbitrage_day["price SD"] / df_arbitrage_day["price mean"]
+)
 ## Simple plot of profit vs. each metric
 for col in df_arbitrage_day.loc[:, df_arbitrage_day.columns.str.startswith("price")]:
     df_arbitrage_day.plot(
@@ -258,7 +275,239 @@ for col in df_arbitrage_day.loc[:, df_arbitrage_day.columns.str.startswith("pric
     )
 
 # %% Task 2.3
-...
+df_sot = pd.DataFrame(index=range(2019, 2024))
+for j in range(1, 11):
+    ### Define the given parameters ###
+    ## SOC limits
+    SOC_min = 0.1
+    SOC_max = 1
+    ## Power and energy limits
+    P_max = 5  # kW
+    capacity = 10  # kWh
+    ## Efficiencies
+    eta_c = eta_d = 0.95
+    # Inverse of the discharge efficiency to avoid division
+    eta_d_inv = 1 / eta_d
+    ## Initial and final SOC
+    C_0 = j / 10 * capacity
+    C_n = j / 10 * capacity
+    ## Data series
+    prices = df_prices["Spot"].values
+    EOD = df_prices["hour"] == 23
+
+    ### Create the optimization variables
+    ## Number of steps in the optimization = number of hours in data
+    n = len(prices)
+    ## Charging and discharging power per hour
+    p_c = cp.Variable(n)
+    p_d = cp.Variable(n)
+    ## Battery state of charge per hour
+    X = cp.Variable(n)
+    ## Boolean variable to indicate if the battery is charging
+    d = cp.Variable(n, boolean=True)
+
+    ### Define the variable to be optimized
+    profit = cp.sum(prices @ (p_d - p_c))
+
+    ### Define the constraints
+    constraints = [
+        ## Power constraints
+        p_c >= 0,
+        p_d >= 0,
+        p_c <= d * P_max,
+        p_d <= (1 - d) * P_max,
+        ## State of charge constraints
+        # Charging in initial time step
+        X[0] == C_0 + p_c[0] * eta_c - p_d[0] * eta_d_inv,
+        # Constrain SOC at End of Day to be C_n every day
+        X[EOD] == C_n,
+        # Final SOC constraint
+        X[-1] == C_n,
+        # SOC limits
+        X >= SOC_min * capacity,
+        X <= SOC_max * capacity,
+    ]
+    ### Make SOC time steps
+    print("making steps")
+    for i in range(1, n):
+        constraints += [
+            X[i] == X[i - 1] + p_c[i] * eta_c - p_d[i] * eta_d_inv,
+        ]
+    ### Create the optimization problem
+    print("making problem")
+    problem = cp.Problem(cp.Maximize(profit), constraints)
+    ### Solve the optimization problem
+    print("solving")
+    problem.solve()
+    print("done")
+    df_temp = pd.DataFrame(
+        {
+            "Charging Power [kW]": p_c.value,
+            "Discharging Power [kW]": p_d.value,
+            "State of Charge [kWh]": X.value,
+        }
+    )
+    ## Calculate profits of each hour
+    df_temp["Profit [DKK]"] = (
+        -df_temp["Charging Power [kW]"] * prices
+        + df_temp["Discharging Power [kW]"] * prices
+    )
+    df_temp["Price [DKK/kWh]"] = prices
+    df_temp["Time"] = df_prices["HourDK"]
+    df_sot["{}%".format(j * 10)] = (
+        df_temp["Profit [DKK]"].groupby(df_temp["Time"].dt.year).sum().values
+    )
+    print(df_sot)
+
+# %% Task 2.3 plot
+plt.figure()
+# Grouped bar chart with the profit for each charging level
+x = len(df_sot.columns)
+bars = len(df_sot.index)
+width = 1 / (bars + 1)
+multiplier = 0
+for year in df_sot.index:
+    plt.bar(
+        np.arange(x) + width * (multiplier - (bars - 1) / 2),
+        df_sot.loc[year].values,
+        width=width,
+        label=year,
+    )
+    multiplier += 1
+plt.xticks(np.arange(x), df_sot.columns)
+plt.xlabel("$C_n$ as a percentage of capacity")
+plt.ylabel("Profit [DKK]")
+plt.title("Yearly profit from arbitrage at different $C_n$ constraints")
+plt.legend(title="Year", loc="upper center", bbox_to_anchor=(0.5, -0.15), ncol=5)
+plt.grid(axis="y", linestyle=":", linewidth=0.7, alpha=0.8)
+plt.tight_layout()
+plt.show()
+
+# %% Task 2.3 dynamic SOT
+### Define the given parameters ###
+## SOC limits
+SOC_min = 0.1
+SOC_max = 1
+## Power and energy limits
+P_max = 5  # kW
+capacity = 10  # kWh
+## Efficiencies
+eta_c = eta_d = 0.95
+# Inverse of the discharge efficiency to avoid division
+eta_d_inv = 1 / eta_d
+## Initial and final SOC
+C_0 = 0.5 * capacity
+## Data series
+prices = df_prices["Spot"].values
+EOD = df_prices["hour"] == 23
+
+### Create the optimization variables
+## Number of steps in the optimization = number of hours in data
+n = len(prices)
+## Charging and discharging power per hour
+p_c = cp.Variable(n)
+p_d = cp.Variable(n)
+## Battery state of charge per hour
+X = cp.Variable(n)
+## Boolean variable to indicate if the battery is charging
+d = cp.Variable(n, boolean=True)
+
+### Define the variable to be optimized
+profit = cp.sum(prices @ (p_d - p_c))
+
+### Define the constraints
+constraints = [
+    ## Power constraints
+    p_c >= 0,
+    p_d >= 0,
+    p_c <= d * P_max,
+    p_d <= (1 - d) * P_max,
+    ## State of charge constraints
+    # Charging in initial time step
+    X[0] == C_0 + p_c[0] * eta_c - p_d[0] * eta_d_inv,
+    # SOC limits
+    X >= SOC_min * capacity,
+    X <= SOC_max * capacity,
+]
+### Make SOC time steps
+print("making steps")
+for i in range(1, n):
+    constraints += [
+        X[i] == X[i - 1] + p_c[i] * eta_c - p_d[i] * eta_d_inv,
+    ]
+### Create the optimization problem
+print("making problem")
+problem = cp.Problem(cp.Maximize(profit), constraints)
+### Solve the optimization problem
+print("solving")
+problem.solve()
+print("done")
+
+# %% Task 2.3 dynamic SOT data analysis
+### Create the dataframe with the optimization results ###
+df_dynamic = pd.DataFrame(
+    {
+        "Charging Power [kW]": p_c.value,
+        "Discharging Power [kW]": p_d.value,
+        "State of Charge [kWh]": X.value,
+    }
+)
+## Calculate profits of each hour
+df_dynamic["Profit [DKK]"] = (
+    -df_dynamic["Charging Power [kW]"] * prices
+    + df_dynamic["Discharging Power [kW]"] * prices
+)
+df_dynamic["Price [DKK/kWh]"] = prices
+df_dynamic["Time"] = df_prices["HourDK"]
+df_sot["dynamic"] = (
+    df_dynamic["Profit [DKK]"].groupby(df_dynamic["Time"].dt.year).sum().values
+)
+X[EOD].value
+
+plt.figure(figsize=(8, 5))
+# Grouped bar chart with the profit for each charging level
+x = len(df_sot.columns)
+bars = len(df_sot.index)
+width = 1 / (bars + 1)
+multiplier = 0
+for year in df_sot.index:
+    plt.bar(
+        np.arange(x) + width * (multiplier - (bars - 1) / 2),
+        df_sot.loc[year].values,
+        width=width,
+        label=year,
+    )
+    multiplier += 1
+plt.xticks(np.arange(x), df_sot.columns)
+plt.xlabel("$C_n$ as a percentage of capacity", fontdict={"fontsize": 12})
+plt.ylabel("Profit [DKK]", fontdict={"fontsize": 12})
+plt.title(
+    "Yearly profit from arbitrage at different $C_n$ constraints",
+    fontdict={"fontsize": 14},
+)
+plt.legend(title="Year", loc="upper center", bbox_to_anchor=(0.5, -0.15), ncol=5)
+plt.grid(axis="y", linestyle=":", linewidth=0.7, alpha=0.8)
+plt.tight_layout()
+plt.show()
+
+plt.figure()
+for year in df_sot.index:
+    df_temp = df_dynamic.loc[
+        (df_dynamic["Time"].dt.year == year) & (df_dynamic["Time"].dt.hour == 23)
+    ]
+    plt.boxplot(
+        df_temp["State of Charge [kWh]"] / capacity * 100,
+        positions=[year],
+        widths=0.6,
+        patch_artist=True,
+    )
+plt.xlabel("Year", fontdict={"fontsize": 12})
+plt.ylabel("State of Charge [%]", fontdict={"fontsize": 12})
+plt.yticks(np.arange(0, 101, 10))
+plt.title("Dynamic State of Charge at the end of the day", fontdict={"fontsize": 14})
+plt.grid(axis="y", linestyle=":", linewidth=0.7, alpha=0.8)
+plt.tight_layout()
+plt.show()
 
 # %% Task 3.1
 ### Load Data ###
@@ -440,8 +689,7 @@ constraints = [
     p_d <= P_max,
     ## Relationship between import/export and charging/discharging
     ## Sum of battery power and shortage/surplus must equal import/export
-    imp == (p_c - p_d + shortage),
-    exp == (p_d - p_c + surplus),
+    surplus + p_d + imp == p_c + shortage + exp,
     ## State of charge constraints
     # Charging in initial time step
     X[0] == C_0 + p_c[0] * eta_c - p_d[0] * eta_d_inv,
@@ -582,4 +830,70 @@ ax5.set_xticks(range(pltrange[0], pltrange[1] + 1, 4))
 # Show the plot
 plt.tight_layout()
 plt.show()
+# %% Task 3.4
+### Define the given parameters ###
+SOC_min = 0.1
+SOC_max = 1
+P_max = 5  # kW
+capacity = 10  # kWh
+eta_c = eta_d = 0.95
+C_0 = 0.5 * capacity
+C_n = 0.5 * capacity
+eta_d_inv = 1 / eta_d
+
+sell = df_prices["Sell"].values
+buy = df_prices["Buy"].values
+net = df_prices["Consumption"].values
+
+surplus = (-net).clip(min=0)
+shortage = (net).clip(min=0)
+
+EOD = df_prices["HourDK"].dt.hour == 23
+
+n = len(net)
+p_c = cp.Variable(n)
+p_d = cp.Variable(n)
+imp = cp.Variable(n)
+exp = cp.Variable(n)
+X = cp.Variable(n)
+
+cost = cp.sum(-exp @ sell + imp @ buy)
+
+constraints = [
+    p_c >= 0,
+    p_d >= 0,
+    imp >= 0,
+    exp >= 0,
+    p_c <= P_max,
+    p_d <= P_max,
+    surplus + p_d + imp == shortage + p_c + exp,
+    X[0] == C_0 + p_c[0] * eta_c - p_d[0] * eta_d_inv,
+    X[EOD] == C_n,
+    X[-1] == C_n,
+    X >= SOC_min * capacity,
+    X <= SOC_max * capacity,
+]
+print("making steps")
+for i in range(1, n):
+    constraints += [X[i] == X[i - 1] + p_c[i] * eta_c - p_d[i] * eta_d_inv]
+print("making problem")
+problem = cp.Problem(cp.Minimize(cost), constraints)
+print("solving")
+problem.solve()
+print("done")
+print(problem.status)
 # %%
+
+df_arb_battery = pd.DataFrame({"Import power": imp.value, "Export power": exp.value})
+
+df_arb_battery["Cost"] = (
+    df_arb_battery["Import power"] * buy - df_arb_battery["Export power"] * buy
+)
+df_arb_battery["Time"] = df_prices["HourDK"]
+
+
+plt.figure()
+df_arb_battery
+df_arb_battery["cumsum"] = df_arb_battery["Cost"].cumsum()
+plt.plot(df_arb_battery["Time"], df_arb_battery["cumsum"])
+plt.show()
